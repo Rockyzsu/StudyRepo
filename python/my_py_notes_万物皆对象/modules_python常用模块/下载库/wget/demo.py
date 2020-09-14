51fabae1386dbee77b0fc075f89d87eeabc17970
Rockyzsu <jinweizsu@gmail.com>
2020-09-14 19:04:55 +0800

    python basic

---
+++ python/my_py_notes_万物皆对象/modules_python常用模块/下载库/wget/demo.py
@@ -0,0 +1,13 @@
+# coding:utf-8
+
+'''
+@author = super_fazai
+@File    : demo.py
+@connect : superonesfazai@gmail.com
+'''
+
+import wget
+
+url = 'https://haitao.nosdn1.127.net/imq35qx294_800_800.jpg'
+file_name = wget.download(url)
+print(file_name)
